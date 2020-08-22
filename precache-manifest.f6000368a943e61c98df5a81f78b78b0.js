77bcf0933a2637b6955b33d9d399c70eaef0a424
Theo <symplytheo@gmail.com>
2020-08-22 21:05:52 +0100

    Updates

---
+++ precache-manifest.f6000368a943e61c98df5a81f78b78b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25aba5d7058b807f015d23fc9ddff5f2",
+    "url": "/podcastly/index.html"
+  },
+  {
+    "revision": "2a63b701ca3fc49af227",
+    "url": "/podcastly/static/css/main.187526f9.chunk.css"
+  },
+  {
+    "revision": "97b44d676bb59a929d37",
+    "url": "/podcastly/static/js/2.87841bc7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/podcastly/static/js/2.87841bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a63b701ca3fc49af227",
+    "url": "/podcastly/static/js/main.8cc74c24.chunk.js"
+  },
+  {
+    "revision": "8224a71e54934f9ec014",
+    "url": "/podcastly/static/js/runtime-main.777297a8.js"
+  },
+  {
+    "revision": "25df958a283b471e6f64bc73466ee5f2",
+    "url": "/podcastly/static/media/laptop-1.25df958a.jfif"
+  },
+  {
+    "revision": "aba139d9edf33fd6e113d74ec5c53525",
+    "url": "/podcastly/static/media/laptop-2.aba139d9.jpg"
+  },
+  {
+    "revision": "7642b6cdc95cfd6b11f48e5470c185ea",
+    "url": "/podcastly/static/media/laptop-3.7642b6cd.jpg"
+  }
+]);
